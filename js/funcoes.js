e617f6ef937cd792fa5462434ac1d2ca6010d11e
amanda <amandavilelaalmeida@gmail.com>
2016-02-01 02:12:01 -0200

    Includes first version files

---
+++ js/funcoes.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	$(document).ready(function(){
+		$('a[href^="#"]').on('click',function (e) {
+		    e.preventDefault();
+
+		    var target = this.hash;
+		    var $target = $(target);
+
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top
+		    }, 500, 'swing', function () {
+		        window.location.hash = target;
+		    });
+		});
+	});
+
+	$('#enviar').click(function(){
+		var nome = $('#nome').val();
+		var email = $('#email').val();
+		var mensagem = $('#mensagem').val();
+
+		$.ajax({
+			type: "POST",
+			url: "includes/envia.php",
+			data: "nome=" + nome + "&email=" + email + "&mensagem=" + mensagem,
+			success: function(data){
+				$('#retorno').html(data);
+			},
+			beforeSend: function(){
+				$('.loading').fadeIn('fast');
+			},
+			complete: function(){
+				$('.loading').fadeOut('fast');
+			}
+		});
+		return false;
+	});
+});
